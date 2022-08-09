f063dad8dd566ea35a4937c072205c169567540e
AndreiHulpoi <hulpoiandrei@gmail.com>
2022-08-09 13:41:06 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+'use strict';
+
+let number = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  //NO INPUT
+  if (!guess) {
+    displayMessage('Please enter a number!');
+    //WHEN GUESS IS RIGHT
+  } else if (guess === number) {
+    displayMessage('Correct Number');
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = number;
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+    //WHEN GUESS IS WRONG
+  } else if (guess !== number) {
+    if (score > 1) {
+      displayMessage(guess < number ? 'Go higher!' : 'Go lower!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You Lost!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  number = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.score').textContent = score;
+  displayMessage('Start guessing...');
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+});
